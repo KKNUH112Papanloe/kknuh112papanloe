583f9da324155aa2e6348ddc691d28bc77f12592
KKNUH112Papanloe <kkndesa1123@gmail.com>
2024-08-05 23:06:49 +0800

    script.js

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
-$(document).ready(function(){
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
+$(document).ready(function() {
+    $(window).scroll(function() {
+        // sticky navbar on scroll script
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
         } else {
             $('.navbar').removeClass("sticky");
         }
 
-        if(this.scrollY > 500){
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
         } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    // slide-up script
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({ scrollTop: 0 });
+        // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $('.navbar .menu li a').click(function() {
+        // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
+    // toggle menu/navbar script
+    $('.menu-btn').click(function() {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
     var typed = new Typed(".typing", {
         strings: ["Baru", "Perubahan"],
         typeSpeed: 120,
@@ -41,6 +48,7 @@ $(document).ready(function(){
         loop: true
     });
 
+    // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
